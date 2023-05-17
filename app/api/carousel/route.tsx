df44884c4dd87aea19ea74021edc0a09508d6f64
Leorio21 <Ptitlue@gmail.com>
2023-05-17 09:53:49 +0200

    Fix: dossier images back

--- app/api/carousel/route.tsx
+++ app/api/carousel/route.tsx
@@ -7,7 +7,7 @@ export  async function GET(): Promise<any> {
 	const imgBdd: string[] = [];
 	let data: string[] = [];
 	try {
-		data = await readdir(path.join(__dirname, "./images/carousel"));
+		data = await readdir(path.join(__dirname, "/images/carousel"));
 	}
 	catch (error) {
 		console.log(error)

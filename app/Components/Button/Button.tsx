5ad8160188462335060b1af607d6ce82d37e8123
Leorio21 <Ptitlue@gmail.com>
2023-05-27 23:36:45 +0200

    Feat: NavBar
    Mise en responsive de la navBar

--- app/Components/Button/Button.tsx
+++ app/Components/Button/Button.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react"
 import css from "./Button.module.css"
 import Link from "next/link";
@@ -6,12 +7,13 @@ interface ButtonProps {
   inName: string;
   inColor: string;
 	inLinkRef?: string;
+	inClick: () => void;
 }
 
-export default function Button({inName, inColor, inLinkRef = "/"}: ButtonProps): JSX.Element {
+export default function Button({inName, inColor, inLinkRef = "/", inClick}: ButtonProps): JSX.Element {
 	return (
 		<Link href={inLinkRef}>
-			<div className={`${css.button} ${css[inColor]}`}>{inName}</div>
+			<div className={`${css.button} ${css[inColor]}`} onClick={() => {inClick()}}>{inName}</div>
 		</Link>
 	)
 }
--- app/Components/NavBar/NavBar.tsx
+++ app/Components/NavBar/NavBar.tsx
@@ -1,14 +1,31 @@
-import React from "react"
-import css from "./NavBar.module.css"
+"use client"
+import React, { useRef } from "react"
+import style from "./NavBar.module.css"
 import Button from "../Button/Button"
+import { Bars3Icon } from "@heroicons/react/20/solid"
 
 export default function NavBar(): JSX.Element {
+
+	const menuIconRef = useRef<HTMLDivElement>(null);
+
+	const setVisible = (): void => {
+		if(menuIconRef.current !== null) {
+			if (menuIconRef.current?.classList.contains(style.menu_visible)) {
+				menuIconRef.current.classList.remove(style.menu_visible)
+			} else {
+				menuIconRef.current.classList.add(style.menu_visible)
+			}
+		}
+	}
 	return (
-		<div className={css.container}>
-			<Button inName={"Accueil"} inColor={"blue"} inLinkRef="/"/>
-			<Button inName={"Projets"} inColor={"yellow"} inLinkRef="/projects"/>
-			<Button inName={"Expériences"} inColor={"purple"} inLinkRef="/experiences"/>
-			<Button inName={"Contact"} inColor={"green"} inLinkRef="/contact"/>
-		</div>
+		<>
+			<Bars3Icon className={style.button_menu} onClick={()=>{setVisible()}}/>
+			<div className={style.container} ref={menuIconRef}>
+				<Button inName={"Accueil"} inColor={"blue"} inLinkRef="/" inClick={()=>{setVisible()}} />
+				<Button inName={"Projets"} inColor={"yellow"} inLinkRef="/projects" inClick={()=>{setVisible()}} />
+				<Button inName={"Expériences"} inColor={"purple"} inLinkRef="/experiences" inClick={()=>{setVisible()}} />
+				<Button inName={"Contact"} inColor={"green"} inLinkRef="/contact" inClick={()=>{setVisible()}} />
+			</div>
+		</>
 	)
 }

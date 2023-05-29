1c077cc03b86788615e841fefc5a30a46c3ae600
Leorio21 <Ptitlue@gmail.com>
2023-05-29 11:17:46 +0200

    Feat: FormButton
    Ajout des bouton de formulaire

--- app/Components/Form/ContactForm/ContactForm.tsx
+++ app/Components/Form/ContactForm/ContactForm.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import Input from "../Input/Input";
 import TextArea from "../TextArea/TextArea";
 import style from "./ContactForm.module.css";
-// import { useNotify } from "@/app/Hooks/Notify/useNotify";
+import { useNotify } from "@/app/Hooks/Notify/useNotify";
+import ButtonForm from "../ButtonForm/ButtonForm";
 
 export default function ContactForm(): JSX.Element {
-	// const { setNotify, NotifyContainer } = useNotify();
+	const { setNotify, NotifyContainer } = useNotify();
+	const [color, setColor] = useState<"success" | "error" | "warning">("success");
 
-	// const click = (): void => {
-	// 	setNotify("Test ok !")
-	// }
+	const click = (): void => {
+		setColor("error");
+		setNotify("Test ok !")
+	}
 
 	return (
 		<>
@@ -17,10 +21,15 @@ export default function ContactForm(): JSX.Element {
 				<Input inId="lname" inType="text" inLabel={"Nom"} inGridPosition={"lastName"}	/>
 				<Input inId="fname" inType="text" inLabel={"Prénom"} inGridPosition={"firstName"}	/>
 				<Input inId="email" inType="email" inLabel={"Em@il"} inGridPosition={"email"}	/>
-				<Input inId="sujet" inType="text" inLabel={"Sujet"} inGridPosition={"subject"}	/>
+				<Input inId="tel" inType="tel" inLabel={"Téléphone"} inGridPosition={"tel"}	/>
+				<Input inId="sujet" inType="text" inLabel={"Objet"} inGridPosition={"subject"}	/>
 				<TextArea inId="message" inLabel={"Message"} inGridPosition={"message"} />
+				<div className={style.buttons}>
+					<ButtonForm inType="reset" inValue="Réinitialiser" inColor="red" inClick={click} />
+					<ButtonForm inType="button" inValue="Envoyer" inColor="green" inClick={click} />
+				</div>
 			</form>
-			{/* <NotifyContainer></NotifyContainer> */}
+			<NotifyContainer color={color}></NotifyContainer>
 		</>
 	)
 }

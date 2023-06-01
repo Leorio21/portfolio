60b5971fd144166707bdaf2e5c1dbc6714943294
Leorio21 <Ptitlue@gmail.com>
2023-06-01 15:38:11 +0200

    Fix: changement de dep mail
    Remplacement de nodemailer par mailjet

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -1,31 +1,37 @@
 import { NextResponse, NextRequest } from "next/server";
-import nodemailer from "nodemailer";
 import { IFormValues} from "../../Interfaces/Interfaces";
+import Mailjet from "node-mailjet";
 
 export async function POST(request: NextRequest) {
 	const body: IFormValues = await request.json()
 
-	const transporter = nodemailer.createTransport({
-		service: process.env.SERVICE_MAIL,
-		auth: {
-			user: process.env.USER_MAIL,
-			pass: process.env.MDP_MAIL,
-			clientId: process.env.OAUTH2_ID,
-			clientSecret: process.env.OAUTH2_TOKEN,
-		},
+	const mailjet = new Mailjet({
+		apiKey: process.env.API_KEY,
+		apiSecret: process.env.SECRET_KEY
 	});
 
-	const mail = {
-		from: `${body.firstName} ${body.lastName} <${body.email}>`,
-		to: process.env.DESTINATION_MAIL,
-		subject: body.subject,
-		text: `${body.message} \n\n----------\n${body.firstName} ${body.lastName}\n${body.phone}`
-	};
-	
-	transporter.sendMail(mail, (error: any, info: { response: string; }) => {  
-		if (error) {  
-			return NextResponse.json({ message: error}, { status: 500 });  
-		}
+	const mail = mailjet.post("send", { version: "v3.1" }).request({
+		Messages: [
+			{
+				From: {
+					Email: process.env.DESTINATION_MAIL,
+					Name: "Me",
+				},
+				To: [
+					{
+						Email: process.env.DESTINATION_MAIL,
+						Name: "Me",
+					},
+				],
+				Subject: body.subject,
+				TextPart: `${body.message}\n\n-----\n${body.firstName} ${body.lastName}\n${body.phone}`,
+			},
+		],
 	})
+	try {
+		await mail
+	} catch (error) {
+		return NextResponse.json({ message: error}, { status: 500 });
+	}
 	return NextResponse.json({ message: "Mail envoyé"}, { status: 200 });  
 }

3b42dcb233982fed1d4f99c9acbab3ae40de26dd
Leorio21 <Ptitlue@gmail.com>
2023-06-03 00:23:07 +0200

    Style: Correction css page accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import AnimatedText from "./Components/AnimatedText/AnimatedText"
+import style from "./page.module.css"
 
 export default function Home(): JSX.Element {
 	return (
-		<>
+		<div className={style.container}>
 			<AnimatedText inTextStart={"$" + "{name}"} inTextEnd="Jérôme Lefeuvre" />
 			<AnimatedText inTextStart={"$" + "{profession}"} inTextEnd="Développeur Web" inStartDelay={500} />
-		</>
+		</div>
 	)
 }

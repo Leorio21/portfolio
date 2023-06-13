666704c89af449034c2befc29f5590f160303f89
Leorio21 <Ptitlue@gmail.com>
2023-06-13 10:43:46 +0200

    Fix: menu burger

--- app/Components/NavBar/NavBar.tsx
+++ app/Components/NavBar/NavBar.tsx
@@ -1,34 +1,22 @@
 "use client"
-import React, { useEffect, useRef, useState } from "react";
-import { useWindowSize } from "@/app/Hooks/WindowSize/useWindowsSize";
-import style from "./NavBar.module.css";
-import Button from "../Button/Button";
-import { Bars3Icon } from "@heroicons/react/20/solid";
+import React, { useRef } from "react"
+import style from "./NavBar.module.css"
+import Button from "../Button/Button"
+import { Bars3Icon } from "@heroicons/react/20/solid"
 
 export default function NavBar(): JSX.Element {
 
 	const menuIconRef = useRef<HTMLDivElement>(null);
-	const [isActiveBurger, setIsActiveBurger] = useState(false);
-	const screenWidth = useWindowSize().width;
 
 	const setVisible = (): void => {
-		if(menuIconRef.current !== null && isActiveBurger) {
+		if(menuIconRef.current !== null && window.innerWidth <= 800) {
 			if (menuIconRef.current?.classList.contains(style.menu_visible)) {
-				menuIconRef.current.classList.remove(style.menu_visible);
+				menuIconRef.current.classList.remove(style.menu_visible)
 			} else {
-				menuIconRef.current.classList.add(style.menu_visible);
+				menuIconRef.current.classList.add(style.menu_visible)
 			}
 		}
 	}
-
-	useEffect(() => {
-		if (screenWidth <= 800) {
-			setIsActiveBurger(true);
-		} else {
-			setIsActiveBurger(false);
-		}
-	}, [screenWidth]);
-
 	return (
 		<>
 			<Bars3Icon className={style.button_menu} onClick={()=>{setVisible()}}/>
